b6a73697c63b434ff7e72f4fba8dda226f7c912d
azizdevfull <aziz16110904@gmail.com>
2022-11-15 10:55:09 +0500

    completed setup db to mysql

--- config.php
+++ config.php
@@ -1,1 +1,14 @@
 
+<?php
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'crud-php';
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connect Filed" . $conn->connect_error);
+    }
+
+    ?>
